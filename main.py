ce2c2915bdb6c1575e752a8495991212f6de8235
jinchenglee <jincheng.li@gmail.com>
2017-10-16 23:29:08 -0700

    Started adding codes into TODO gaps.

--- main.py
+++ main.py
@@ -32,8 +32,17 @@ def load_vgg(sess, vgg_path):
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
+
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+
+    graph = tf.get_default_graph()
+    image_input = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3_out = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4_out = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7_out = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
     
-    return None, None, None, None, None
+    return image_input, keep_prob, layer3_out, layer4_out, layer7_out
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -47,7 +56,34 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    return None
+    # 1x1 convolution of vgg encoder last layer
+    conv_1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding='same', 
+                    kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    # Scaling up by 2 of conv_1x1
+    deconv_1 = tf.layers.conv2d_transpose(conv_1x1, num_classes, 4, 2, padding='same',
+                    kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    # 1x1 convolution of vgg encoder layer 4 output
+    vgg_layer4_conv_1x1 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, padding='same',
+                    kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    # combine the two
+    combine_1 = tf.add(deconv_1, vgg_layer4_conv_1x1)
+    
+
+    # Scaling up by 2 of combine_1 
+    deconv_2 = tf.layers.conv2d_transpose(combine_1, num_classes, 4, 2, padding='same',
+                    kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    # 1x1 convolution of vgg encoder layer 3 output
+    vgg_layer3_conv_1x1 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, padding='same',
+                    kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    # combine the two
+    combine_2 = tf.add(deconv_2, vgg_layer3_conv_1x1)
+    
+    with tf.name_scope('encode_final_layers'):
+        out = tf.layers.conv2d_transpose(combine_2, num_classes, 16, 8, padding='same',
+                    kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+        tf.summary.histogram('encode_final_upsample', out)
+    return out
 tests.test_layers(layers)
 
 
